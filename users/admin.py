622bc0ca2a47d993d4d839806d6191a98d96f981
reza <qrezahosseiny@gmail.com>
2026-06-06 10:38:03 +0330

    admin connet to db

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ["username", "email", "phone_number", "is_staff", "is_superuser"]
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("اطلاعات تکمیلی", {"fields": ("phone_number",)}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("اطلاعات تکمیلی", {"fields": ("phone_number",)}),
+    )
