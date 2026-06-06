b895db6af950e925bf94ed047e82fe5620863c4d
reza <qrezahosseiny@gmail.com>
2026-06-06 11:11:31 +0330

    add jwt token to urls

--- sweetshop/urls.py
+++ sweetshop/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include("products.urls")),
+    path("api/products/", include("products.urls")),
     path('api/users/', include('users.urls')),
 ]
 
